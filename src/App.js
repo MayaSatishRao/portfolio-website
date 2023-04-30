2621956ee4010f33b9219b3f03f9f6a42706f4ad
Maya S Rao <mayasrao25@gmail.com>
2023-04-30 21:24:45 +0530

    added options

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
 import SearchBar from './components/SearchBar/SearchBar';
+import Options from './components/options/Options';
 function App() {
   return (
+    <>
     <SearchBar/>    
+     <Options/>
+    </>
   );
 }
 
