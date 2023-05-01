e58e56d9ec7f4088c543cf741f15dcbf2b3933bc
Maya S Rao <mayasrao25@gmail.com>
2023-05-01 14:42:53 +0530

    added about and profile

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 import SearchBar from './components/SearchBar/SearchBar';
 import Options from './components/options/Options';
+import Education from "./components/education/Education"
 function App() {
   return (
     <>
     <SearchBar/>    
      <Options/>
+     <Education/>
     </>
   );
 }
